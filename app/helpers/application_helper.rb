8afe4831ec3b1dd836e2b6fc17ef1e374aa14eda
pbmolini <pbmolini@fbk.eu>
2017-09-06 11:49:10 +0200

    nice flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
   def localised_language_name(locale=I18n.locale)
     I18nData.languages(I18n.locale)[locale.to_s.upcase]
   end
+
+  def flash_alert_type(name)
+    case name
+    when 'notice'
+      'primary'
+    when 'alert'
+      'danger'
+    else
+      'secondary'
+    end
+  end
 end
