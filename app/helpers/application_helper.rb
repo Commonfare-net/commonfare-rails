027b45b1a21bb122aacda3ef4f2a8043a61b976c
pbmolini <pbmolini@fbk.eu>
2017-09-06 14:22:37 +0200

    fixes in navbar and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,9 @@ module ApplicationHelper
     I18n.available_locales - [ I18n.locale ]
   end
 
-  def localised_language_name(locale=I18n.locale)
-    I18nData.languages(I18n.locale)[locale.to_s.upcase]
+  def localised_language_name(translation_locale=I18n.locale, language_locale=I18n.locale)
+    # e.g. I18nData.languages(:it)['HR'] => 'Croato'
+    I18nData.languages(translation_locale)[language_locale.to_s.upcase]
   end
 
   def flash_alert_type(name)
