4706c2327076bba12be21682da0c046e137a1d4d
pbmolini <pbmolini@fbk.eu>
2017-09-11 16:40:40 +0200

    More fixes on Stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,4 +4,8 @@ class Story < ApplicationRecord
   translates :title, :content
 
   validates :title, :content, :place, presence: true
+
+  def translated_in?(locale)
+    title_translations.keys.include? locale.to_s
+  end
 end
