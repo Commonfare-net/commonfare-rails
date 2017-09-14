f209c5e1db5d3f946c94083b4e396438ae3a3b65
pbmolini <pbmolini@fbk.eu>
2017-09-14 17:55:00 +0200

    Fixed an annoyng issue due to a bog in the globalize gem

--- config/initializers/globalize.rb
+++ config/initializers/globalize.rb
@@ -1,6 +1,23 @@
+# This should be the correct way to manage fallbacks
+# but apparently it doesn't work with Rails 5
+# see https://github.com/globalize/globalize/issues/615
 # Globalize.fallbacks = {
-#   en: [:it, :nl, :hr],
-#   it: [:en, :nl, :hr],
-#   nl: [:en, :it, :hr],
-#   hr: [:en, :it, :nl],
+#   en: %i[en it nl hr],
+#   it: %i[it en nl hr],
+#   nl: %i[nl en it hr],
+#   hr: %i[hr en it nl],
 # }
+
+# This is how it is solved in https://github.com/globalize/globalize/issues/615
+module I18n
+  FALLBACKS = {
+    en: %i[en it nl hr],
+    it: %i[it en nl hr],
+    nl: %i[nl en it hr],
+    hr: %i[hr en it nl],
+  }.freeze
+
+  def self.fallbacks
+    FALLBACKS
+  end
+end
