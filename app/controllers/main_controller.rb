5b7565fe9666daf35e6a2be7b2626a72d31d1127
pbmolini <pbmolini@fbk.eu>
2017-09-19 12:26:57 +0200

    better serp and empty search correctly managed

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -13,11 +13,14 @@ class MainController < ApplicationController
     # To search only in selected languages text use this
     # @stories  = Story.with_translations(:it)ransack(translations_title_cont: params[:q]).result(distinct: true).limit(5)
     # To search in all languages use this
+    @q = params[:q]
+    # no search key? Get back to where you once belonged (cit. The Beatles)
+    redirect_back(fallback_location: root_path) if @q.blank?
     @stories  = Story.ransack(
-                        translations_title_or_translations_content_cont: params[:q]
+                        translations_title_or_translations_content_or_tags_name_cont: @q
                       ).result(distinct: true)
     @tags     = Tag.ransack(
-                        name_cont: params[:q]
+                        name_cont: @q
                       ).result(distinct: true)
   end
 end
