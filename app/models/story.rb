1cc68311d025c0c7f9f579934e5a97f8239b878c
pbmolini <pbmolini@fbk.eu>
2017-09-27 18:11:02 +0200

    destroy comments when a story is destroyed

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,7 @@
 class Story < ApplicationRecord
   belongs_to :commoner
   has_and_belongs_to_many :tags
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
   has_many :images, as: :imageable, dependent: :destroy
 
   translates :title, :content
