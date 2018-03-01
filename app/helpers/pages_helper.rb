a4dae04f65d324d6ed05ece41b23b4ad3d2cd5de
pbmolini <pbmolini@fbk.eu>
2018-03-01 17:30:22 +0100

    Story types listed in home page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,4 +3,26 @@ module PagesHelper
     locale = %i(it hr nl).include?(I18n.locale) ? I18n.locale : 'en'
     "pages/#{locale}/#{page_name}"
   end
+
+  def story_type_title(story_type)
+    case story_type
+    when :commoners_voice
+      s_('Home title|Commoners Voices')
+    when :good_practice
+      s_('Home title|Good Practices')
+    when :welfare_provision
+      s_('Home title|Public Benefits')
+    end
+  end
+
+  def story_type_subtitle(story_type)
+    case story_type
+    when :commoners_voice
+      s_('Home text|A collection of stories for, with and by people.')
+    when :good_practice
+      s_('Home text|Bottom-up initiatives to connect and inspire.')
+    when :welfare_provision
+      s_('Home text|Get informed about local welfare provisions.')
+    end
+  end
 end
