fdecdd8f2d489ecfdb4a996d1eadf28c60579388
pbmolini <pbmolini@fbk.eu>
2018-03-05 10:53:13 +0100

    Tag page updated

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,6 +4,11 @@ class TagsController < ApplicationController
   authorize_resource # managed by CanCanCan
   def show
     @tag = Tag.friendly.find params[:id]
-    @stories = @tag.stories
+    stories = @tag.stories
+    @story_types_and_lists = {
+      commoners_voice: stories.commoners_voice.order('created_at DESC'),
+      good_practice: stories.good_practice.order('created_at DESC'),
+      welfare_provision: stories.welfare_provision.order('created_at DESC')
+    }
   end
 end
