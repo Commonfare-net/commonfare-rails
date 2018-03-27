c28fed56470ce230166a45eca1575c4ab7dddd4c
pbmolini <pbmolini@fbk.eu>
2018-03-27 10:21:36 +0200

    links in messages detected

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -34,7 +34,7 @@ class DiscussionsController < ApplicationController
     binding.pry
     respond_to do |format|
       if @discussion.save
-        format.html { redirect_to @discussion, notice: 'Discussion was successfully created.' }
+        format.html { redirect_to group_discussion_path(@group, @discussion), notice: 'Discussion was successfully created.' }
         format.json { render :show, status: :created, location: @discussion }
       else
         format.html { render :new }
