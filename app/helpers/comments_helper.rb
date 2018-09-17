60085ac2ec43983cb031495d83bfd83a48736381
pbmolini <pbmolini@fbk.eu>
2018-09-17 15:02:37 +0200

    Short explaination of anonymous comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,4 +3,8 @@ module CommentsHelper
     return listing_comments_path(commentable) if commentable.is_a? Listing
     story_comments_path(commentable)
   end
+
+  def anonymous_comment_tooltip_text
+    _('Your profile will not be linked to this comment, but you will still be able to edit or delete the comment.')
+  end
 end
